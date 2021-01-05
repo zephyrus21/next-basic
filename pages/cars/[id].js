4664ff9d96246e22d098d32fe01bb9664a21cd9a
zephyrus21 <pandey.piyus@gmail.com>
2021-01-06 00:30:07 +0530

    up

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,10 +1,45 @@
 import { useRouter } from 'next/router';
 
-const Car = () => {
+import Head from 'next/head';
+
+const Car = ({ car }) => {
   const router = useRouter();
   const { id } = router.query;
 
-  return <h1>Hello {id}</h1>;
+  return (
+    <>
+      <Head>
+        <title>
+          {car.color} {car.id}
+        </title>
+      </Head>
+      <h1>Hello {id}</h1>
+      <img src={car.image} width="300px" />
+    </>
+  );
 };
 
 export default Car;
+
+export const getStaticProps = async ({ params }) => {
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { car: data },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const req = await fetch(`http://localhost:3000/cars.json`);
+  const data = await req.json();
+
+  const paths = data.map((car) => {
+    return { params: { id: car } };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
